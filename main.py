e755f76dedf18e8945bc5234d9d5aeeb9e79b761
Vivek Jangir <113574190+Vivekjangir90@users.noreply.github.com>
2024-05-08 04:06:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ history=[{'parts': [{'text': "Greetings! you are  Vivek Jangir, your AI chatbot 
 
 def get_gemini_response(question,history):
     generation_config = {
-    "temperature": 0.9,
+    "temperature": 0.5,
     "top_p": 1,
     "top_k": 1,
     "max_output_tokens": 2048,
@@ -43,21 +43,23 @@ def get_gemini_response(question,history):
     safety_settings = [
     {
         "category": "HARM_CATEGORY_HARASSMENT",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+        "threshold": "BLOCK_LOW_AND_ABOVE"
     },
     {
         "category": "HARM_CATEGORY_HATE_SPEECH",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+        "threshold": "BLOCK_LOW_AND_ABOVE"
     },
     {
         "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+        "threshold": "BLOCK_LOW_AND_ABOVE"
     },
     {
         "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+        "threshold": "BLOCK_LOW_AND_ABOVE"
     },
     ]
+
+   
 
     model = genai.GenerativeModel(model_name="gemini-1.0-pro",
                                 generation_config=generation_config,
